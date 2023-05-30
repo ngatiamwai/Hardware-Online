07b583d9cc5524bff02ca80750dafb666b989db3
ngatiamwai <98641144+ngatiamwai@users.noreply.github.com>
2023-05-30 16:57:37 +0300

    logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,9 @@
 <?php
 
 session_start();
- 
-$_SESSION = array();
- 
-session_destroy();
- 
-header("location: products.php");
+ $_SESSION = array();
+ session_destroy();
+ header("location: products.php");
 exit;
+
 ?>
